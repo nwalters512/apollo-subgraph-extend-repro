9fdb4abbc6a4c48152b691d42b0c3bf20adf3e41
Nathan Walters <nwalters@nerdwallet.com>
2022-11-09 11:01:44 -0800

    Simplify repro

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # apollo-subgraph-extend-repro
+
+This contains a reproduction of an issue with `printSchemaWithDirectives` from the `@graphql-tools/utils` package.
+
+## Steps to reproduce
+
+- Run `yarn install`
+- Run `node index.js`
+- Observe the output from `printSubgraphSchema` contains `extend type Foo { ... }`, but the output from `printSchemaWithDirectives` is missing the `extend` keyword.
--- index.js
+++ index.js
@@ -2,27 +2,16 @@ const {
   buildSubgraphSchema,
   printSubgraphSchema,
 } = require("@apollo/subgraph");
-// const { printSchemaWithDirectives } = require("@graphql-tools/utils");
+const { printSchemaWithDirectives } = require("@graphql-tools/utils");
 const gql = require("graphql-tag");
-const { addResolversToSchema } = require("@graphql-tools/schema");
 
 const schema = gql`
-  type Foo {
-    bar: String
-  }
   extend type Foo {
-    baz: String
+    bar: String
   }
 `;
 
 const subgraphSchema = buildSubgraphSchema({ typeDefs: [schema] });
-
-// console.log("printSchemaWithDirectives");
-// console.log("==========================");
-// console.log(printSchemaWithDirectives(subgraphSchema));
-// console.log("==========================");
-
-// console.log("");
 
 console.log("printSubgraphSchema");
 console.log("==========================");
@@ -31,19 +20,7 @@ console.log("==========================");
 
 console.log("");
 
-const subgraphSchemaWithResolvers = addResolversToSchema({
-  schema: subgraphSchema,
-  resolvers: {},
-});
-
-// console.log("printSchemaWithDirectives after addResolversToSchema");
-// console.log("==========================");
-// console.log(printSchemaWithDirectives(subgraphSchemaWithResolvers));
-// console.log("==========================");
-
-// console.log("")
-
-console.log("printSubgraphSchema");
+console.log("printSchemaWithDirectives");
 console.log("==========================");
-console.log(printSubgraphSchema(subgraphSchemaWithResolvers));
+console.log(printSchemaWithDirectives(subgraphSchema));
 console.log("==========================");
