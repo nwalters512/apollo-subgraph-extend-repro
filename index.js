0e001eed478e3d70f5ecd56579975db1fc7d19e1
Nathan Walters <nwalters@nerdwallet.com>
2022-11-09 10:55:48 -0800

    Simplify reproduction

--- index.js
+++ index.js
@@ -2,31 +2,48 @@ const {
   buildSubgraphSchema,
   printSubgraphSchema,
 } = require("@apollo/subgraph");
-const { printSchemaWithDirectives } = require("@graphql-tools/utils");
+// const { printSchemaWithDirectives } = require("@graphql-tools/utils");
 const gql = require("graphql-tag");
+const { addResolversToSchema } = require("@graphql-tools/schema");
 
 const schema = gql`
-  extend schema
-    @link(
-      url: "https://specs.apollo.dev/federation/v2.0"
-      import: ["@shareable", "@override", "@key"]
-    )
-
   type Foo {
     bar: String
   }
-
-  extend type Baz {
-    biz: String
+  extend type Foo {
+    baz: String
   }
 `;
 
 const subgraphSchema = buildSubgraphSchema({ typeDefs: [schema] });
 
-console.log("printSchemaWithDirectives");
-console.log(printSchemaWithDirectives(subgraphSchema));
+// console.log("printSchemaWithDirectives");
+// console.log("==========================");
+// console.log(printSchemaWithDirectives(subgraphSchema));
+// console.log("==========================");
 
-console.log("==========================");
+// console.log("");
 
 console.log("printSubgraphSchema");
+console.log("==========================");
 console.log(printSubgraphSchema(subgraphSchema));
+console.log("==========================");
+
+console.log("");
+
+const subgraphSchemaWithResolvers = addResolversToSchema({
+  schema: subgraphSchema,
+  resolvers: {},
+});
+
+// console.log("printSchemaWithDirectives after addResolversToSchema");
+// console.log("==========================");
+// console.log(printSchemaWithDirectives(subgraphSchemaWithResolvers));
+// console.log("==========================");
+
+// console.log("")
+
+console.log("printSubgraphSchema");
+console.log("==========================");
+console.log(printSubgraphSchema(subgraphSchemaWithResolvers));
+console.log("==========================");
